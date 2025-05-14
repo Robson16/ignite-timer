a1c43bfc882c33d5b00a7b3d00de1a01d5bbf4f1
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-05-13 22:43:48 -0300

    Estrutura da aplicação - Configurando temas
    Com o Styled Components, nós conseguimos criar temas para estilizar nossa aplicação de formas diferentes de acordo com o tema escolhido.
    
    Nessa aula vamos fazer um setup inicial de um tema padrão para nossa aplicação, que será onde iremos configurar as cores que vamos utilizar no futuro.

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,20 +6,12 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const buttonVariants = {
-  primary: 'purple',
-  secondary: 'orange',
-  danger: 'red',
-  success: 'green'
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-
-  ${props => {
-    return css`
-      background-color: ${buttonVariants[props.variant]}
-    `
-  }}
+  border-radius: 8px;
+  border: 0;
+  margin: 8px;
+  color: ${props => props.theme.white};
+  background-color: ${props => props.theme.primary};
 `;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,5 @@
+export const defaultTheme = {
+  white: '#fff',
+  primary: '#8257e5',
+  secondary: '#6c757d',
+}
