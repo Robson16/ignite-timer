d7f67ed746bc51226d6212a5ac0f37793bd9c8ea
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-05-17 17:00:50 -0300

    Páginas e rotas - Aprimorando os inputs
    Agora para adicionar detalhes finais e aprimorar ainda mais o funcionamento da aplicação, vamos ver alguns elementos especiais do HTML que nos permitem personalizar as recomendações do Input.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,10 +19,25 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
 
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
-          <TimeInput type="number" id="minutesAmount" placeholder="00" />
+          <TimeInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            min={5}
+            max={60}
+            step={5}
+          />
 
           <span>minutos.</span>
         </FormContainer>
