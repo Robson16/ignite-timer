7c40da51512e819cb5547adf54c80cbdcaf31356
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-08-24 16:50:19 -0300

    Reducers - Salvando estado no storage
    Nessa aula iremos aprender como salvar algumas informações da nossa aplicação no Storage do navegador, para não perdermos os dados dos ciclos caso a página seja atualizada.

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useReducer, useState, type ReactNode } from "react";
+import { differenceInSeconds } from "date-fns";
+import { createContext, useEffect, useReducer, useState, type ReactNode } from "react";
 import { v4 as uuid } from 'uuid';
 import {
   addNewCycleAction,
@@ -32,15 +33,38 @@ export const CyclesContext = createContext({} as CyclesContextType)
 export function CyclesContextProvider(
   { children }: CyclesContextProviderProps
 ) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null
+    },
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state-1.0.0')
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+
+      return initialState
+    }
+  )
 
   const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
